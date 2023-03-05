4c064894cdb9387eacf737e0b49384c1c3fa6476
Boon <laiboonh@gmail.com>
2023-03-05 17:37:14 +0800

    Turn on Typescript

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -4,25 +4,34 @@ import Date from "../../components/date";
 import utilStyles from "../../styles/utils.module.css";
 import { getAllPostIds } from "../../lib/posts";
 import { getPostData } from "../../lib/posts";
+import { GetStaticProps, GetStaticPaths } from "next";
 
-export async function getStaticPaths() {
+export const getStaticPaths: GetStaticPaths = async () => {
   const paths = getAllPostIds();
   return {
     paths,
     fallback: false,
   };
-}
+};
 
-export async function getStaticProps({ params }) {
+export const getStaticProps: GetStaticProps = async ({ params }) => {
   const postData = await getPostData(params.id);
   return {
     props: {
       postData,
     },
   };
-}
+};
 
-export default function Post({ postData }) {
+export default function Post({
+  postData,
+}: {
+  postData: {
+    title: string;
+    date: string;
+    contentHtml: string;
+  };
+}) {
   return (
     <Layout>
       <Head>
